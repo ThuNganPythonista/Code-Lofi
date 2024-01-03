5e6a30b08131bce9e2a962179e936293ceb8ec21
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-01-03 21:54:57 +0700

    12 commit

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 pip install -r requirements.txt
-python manage.py makemigrations
-python manage.py migrate
-python manage.py collectstatic
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
+python3.9 manage.py collectstatic
